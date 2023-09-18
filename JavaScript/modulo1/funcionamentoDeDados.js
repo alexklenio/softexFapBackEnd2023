9009177c03c49cd1a9482b4596a8e573e43ab94a
Alex Lopes <alexlopes.see@gmail.com>
2023-09-17 22:35:21 -0300

    Atividade de JavaScript

---
+++ JavaScript/modulo1/funcionamentoDeDados.js
@@ -0,0 +1,30 @@
+// Declarando as variáveis com valores Undefined
+
+let numero;
+let aprovado;
+let nome;
+let simbolo;
+console.log(numero);
+console.log(aprovado);
+console.log(nome);
+console.log(simbolo);
+
+// Atribuindo valores nulos as variáveis declaradas
+numero = null;
+aprovado = null;
+nome = null;
+simbolo = null;
+console.log(numero);
+console.log(aprovado);
+console.log(nome);
+console.log(simbolo);
+
+// Atribuindo valores do tipo Number, Boolean, String e Symbol
+numero = 10;
+aprovado = true;
+nome = "Alex";
+simbolo = Symbol("Meu simbolo");
+console.log(numero);
+console.log(aprovado);
+console.log(nome);
+console.log(simbolo);
--- Lógica de programação/Lógica de programação - Modulo2 - Code Park 2.js
+++ Lógica de programação/Lógica de programação - Modulo2 - Code Park 2.js
@@ -1,11 +1,16 @@
+/* VisualStudio Code, instalado e com as Extenções Node.js Exec para rodar o node dentro da IDE,
+Extenção Prettier que ajuda na organização e formatação do código escrito,
+Extenção Live Server que  cria um servidor local, possibilitando a visualização e execução do codigo asim que     escrito,
+ TemaDracula, apenas para desacar em cores no codigo de acordo com o que for escrito.*/
+
 //declaração de variáveis
 var nome, idade, altura, maiorDeIdade;
 
 //função recebendo os valores de cada tipo
-nome=prompt("Informe o nome do usuário: ");
-idade=parseInt(prompt("informe a idade do usuário: "));
-altura=parseFloat(prompt("Informe a altura do usuário: "));
-maiorDeIdade=prompt("O usuário é maior de idade? (Sim || Não)").toUpperCase();
+nome = prompt("Informe o nome do usuário: ");
+idade = parseInt(prompt("informe a idade do usuário: "));
+altura = parseFloat(prompt("Informe a altura do usuário: "));
+maiorDeIdade = prompt("O usuário é maior de idade? (Sim || Não)").toUpperCase();
 
 //impressãodos valores no console
 console.log("O nome do usuário é " + nome);
@@ -13,8 +18,8 @@ console.log("A idade do usuário é " + idade + " anos");
 console.log("A altura do usuário é " + altura);
 
 //condicionais para a resposta ser falsa ou verdadeira
-if(maiorDeIdade==="SIM"){
-    console.log("O usuário é maior de idade: " + true);
-}else{
-    console.log("O usuário é maior de idade: " + false);
+if (maiorDeIdade === "SIM") {
+  console.log("O usuário é maior de idade: " + true);
+} else {
+  console.log("O usuário é maior de idade: " + false);
 }
