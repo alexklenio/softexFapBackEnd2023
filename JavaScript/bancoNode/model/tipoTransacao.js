848f64d366de926a92fcead046cf7af012f16040
Alex Lopes <106409874+alexklenio@users.noreply.github.com>
2023-10-10 13:32:03 -0300

    Implementação do Banco Node
    Banco digital

---
+++ JavaScript/bancoNode/model/tipoTransacao.js
@@ -0,0 +1,8 @@
+export const TIPOTRANSACAO = {
+    debitar : "DÉBITO\t",
+    creditar : "CRÉDITO\t",
+    transferir : "TRANSFERÊNCIA",
+    pagar : "PAGAMENTO",
+    render : "RENDIMENTO",
+    cobrarTaxa : "TARIFAÇÃO"
+}
